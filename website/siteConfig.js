0b4ce9ff710e0363d998724b7e98ff8c538eb7da
Katia Punter <katia.punter@27partnerss-MacBook-Pro.local>
2018-12-17 11:55:04 +0000

    first try to add copy button

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -95,6 +95,13 @@ const siteConfig = {
   ogImage: 'img/docusaurus.png',
   twitterImage: 'img/docusaurus.png',
 
+  scripts: [
+    'https://buttons.github.io/buttons.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/2.0.0/clipboard.min.js',
+    '/js/code-block-buttons.js',
+  ],
+  stylesheets: ['/css/code-block-buttons.css']
+
   // Show documentation's last contributor's name.
   // enableUpdateBy: true,
 
